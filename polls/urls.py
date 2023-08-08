561567ef7173fbbba44c68c5e07e3816af704bc7
Tuan Nguyen <trentontuannguyen@gmail.com>
2023-08-08 17:28:58 -0400

    add views and wire them in polls/urls

--- polls/urls.py
+++ polls/urls.py
@@ -2,5 +2,16 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # /polls/
     path("", views.index, name="index"),
+
+    # /polls/:id/
+    path("<int:question_id>", views.detail, name="details"),
+
+    # /polls/:id/results/
+    path("<int:question_id>/results/", views.results, name="results"),
+
+    # /polls/:id/vote/
+    path("<int:question_id>/vote/", views.vote, name="vote"),
+     
 ]
