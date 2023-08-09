82f1ea3adb354451a8d629f153474faa0c171913
Tuan Nguyen <trentontuannguyen@gmail.com>
2023-08-09 16:23:58 -0400

    build the form in detail template and the vote functions in views to handle the post request

--- polls/urls.py
+++ polls/urls.py
@@ -4,12 +4,8 @@ from . import views
 
 app_name = "polls"
 urlpatterns = [
-    # /polls/
     path("", views.index, name="index"),
-    # /polls/:id/
-    path("specifics/<int:question_id>", views.detail, name="details"),
-    # /polls/:id/results/
+    path("<int:question_id>/", views.detail, name="detail"),
     path("<int:question_id>/results/", views.results, name="results"),
-    # /polls/:id/vote/
-    path("<int:question_id>/vote/", views.vote, name="vote"),    
+    path("<int:question_id>/vote/", views.vote, name="vote"),
 ]
